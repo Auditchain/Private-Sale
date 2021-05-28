061510c113eb542aa535935abee8e297a9a59f65
Bogdan Feidur <win2win@protonmail.com>
2021-05-28 10:22:41 -0500

    simplify DAI

--- contracts/DAI.sol
+++ contracts/DAI.sol
@@ -1,66 +1,19 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity =0.8.0;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./Locked.sol";
 
-contract DAI is Locked, ERC20{
+contract DAI is ERC20{
       
-    /// @notice A record of each accounts delegate
-    mapping (address => address) public delegates;
-
-    /// @notice A checkpoint for marking number of votes from a given block
-    struct Checkpoint {
-        uint32 fromBlock;
-        uint96 votes;
-    }
+   
 
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 250000000 * (10**uint256(DECIMALS));
-    address public migrationAgent;
-    uint256 public totalMigrated;
     
-   
     /// @dev Constructor that gives an account all initial tokens.
     constructor(address account) ERC20("Stable Token", "DAI") {
         require(account != address(0), "Crowdsale:constructor - Address can't be 0");
-        _mint(account, INITIAL_SUPPLY);      
-        _setupRole(DEFAULT_ADMIN_ROLE, account);
+        _mint(account, INITIAL_SUPPLY);              
     }
 
-    /// @dev prevent accidental sending of tokens to this token contract
-    /// @param _self - address of this contract
-    modifier notSelf(address _self) {
-        require(
-            _self != address(this),
-            "You are trying to send tokens to this token contract"
-        );
-        _;
-    }
-
-    /// @notice Overwrite parent implementation to add locked verification, notSelf modifiers and call to moveDelegates
-    function transfer(address to, uint256 value)
-        public
-        override
-        isNotLocked(msg.sender, to)
-        notSelf(to)
-        returns (bool)
-    {
-        return super.transfer(to, value);
-    }
-
-   /// @notice Overwrite parent implementation to add locked verification, notSelf modifiers and call to moveDelegates
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) public override isNotLocked(from, to) notSelf(to) returns (bool) {
-        return super.transferFrom(from, to, value);
-    }
-
-
-   
-    
 }
