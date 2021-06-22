35743454f26c912fa51fb94af24176179a367b34
Bogdan Feidur <win2win@protonmail.com>
2021-06-21 19:50:41 -0500

    remove unused imports

--- contracts/DataSubClaim.sol
+++ contracts/DataSubClaim.sol
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.0;
-// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./AuditToken.sol";
 
-// @note this contract can be inherited by Sale contract
-// control release of tokens through even time release based on the inputted duration time interval
+// @note this contract will allow data subscribers to claim their tokens. 
 contract DataSubClaim {
-    // using SafeMath for uint256;
-    // using SafeERC20 for AuditToken;
-
     mapping (address => uint256) public amounts;
     mapping (address => bool) public  redeemed;
 
@@ -19,6 +12,7 @@ contract DataSubClaim {
     event Redeemed (address user, uint256 amount);
 
     constructor(address auditToken) {
+      // example data
         amounts[0x67794670742BA1E53FD04d8bA22a9b4487CE65B4] = 200 * 10e18;        
         amounts[0x4311eD2826C3D4E7c82149fAAEe9FB7f40e05568] = 200 * 10e18;        
         amounts[0xd431134b507d3B6F2742687e14cD9CbA5b6BE0F4] = 100 * 10e18;       
