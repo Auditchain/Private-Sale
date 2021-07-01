98cd6bf007cf0065bdcc0da148ee55b0e458c64e
Bogdan Feidur <win2win@protonmail.com>
2021-07-01 15:05:54 -0500

    enable live DAI

--- contracts/UniswapPriceOracle.sol
+++ contracts/UniswapPriceOracle.sol
@@ -7,8 +7,8 @@ contract UniswapPriceOracle {
   address internal constant UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D ;
 
   IUniswapV2Router02 public uniswapRouter;
-//   address private Dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;  //Main net
-     address public Dai = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;  //Kovan 
+  address private Dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;  //Main net
+    //  address public Dai = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;  //Kovan 
 
   constructor() {
     uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
@@ -22,17 +22,12 @@ function getPathForDAItoEth() private view returns (address[] memory) {
     return path;
 }
 
-function getEstimatedDAIForEth(uint ethAmount) public pure returns (uint256[] memory)
+function getEstimatedDAIForEth(uint ethAmount) public view returns (uint256[] memory)
   {
 
-    //TO DO Uncomment for testnet or main deployment
-    // return uniswapRouter.getAmountsIn(ethAmount, getPathForDAItoEth());
+    return uniswapRouter.getAmountsIn(ethAmount, getPathForDAItoEth());
 
-    // TOD remove when deploying to testnet or main. Used for local testing
-    uint256[] memory retValue = new uint256[](2);
-    retValue[0] = 3000000000000000000000 * ethAmount / 1e18 ;
-    retValue[1] = 1000000000000000000;
-    return retValue;
+    
   }
 
 }
